28a8c095e38c3fcab670f5d319b1ac1b9fb66589
Donisanjaya27 <doni@gmail.co.id>
2024-01-04 15:02:14 +0700

    design login sebelum atomic

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,47 @@ import React from "react";
 import Button from "./components/Elements/Button";
 function App() {
 	return (
-		<div className="flex justify-center bg-blue-600 min-h-screen items-center">
-			<div className="flex gap-x-3">
-				<Button variant="bg-red-700">
+		<div className="flex justify-center min-h-screen items-center">
+			<div className="w-full max-w-xs">
+				<h1 className="text-3xl font-bold mb-2 text-blue-600">
+					Login
+				</h1>
+				<p className="font-medium text-slate-500 mb-8">
 					{" "}
+					Welcome, Please enter your
+					details
+				</p>
+				<form action="">
+					<div className="mb-6">
+						<label
+							htmlFor="email"
+							className="block text-slate-700 text-sm font-bold mb-2"
+						>
+							Email
+						</label>
+						<input
+							type="text"
+							className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder: opacity-50"
+							placeholder="example@email.com"
+						/>
+					</div>
+					<div className="mb-6">
+						<label
+							htmlFor="email"
+							className="block text-slate-700 text-sm font-bold mb-2"
+						>
+							Password
+						</label>
+						<input
+							type="text"
+							className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder: opacity-50"
+							placeholder="****"
+						/>
+					</div>
+				</form>
+				<Button classname="bg-blue-600 w-full">
 					Login
 				</Button>
-				{/* //   variant="bg-red-700"
-      //   text="Login"
-      // /> */}
-
-				<Button variant="bg-slate-700">
-					{" "}
-					Logout
-				</Button>
-				<Button variant="bg-black"> </Button>
 			</div>
 		</div>
 	);
