f94a98b14de91bdb174bd4b0806fe494b6f0e459
Donisanjaya27 <doni@gmail.co.id>
2024-01-04 13:45:52 +0700

    button

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,12 @@ function App() {
 
 	return (
 		<div className="flex justify-center bg-blue-600 min-h-screen items-center">
-			<div> Hello Word</div>
+			<button
+				className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+				type="submit"
+			>
+				Buy now
+			</button>
 		</div>
 	);
 }
