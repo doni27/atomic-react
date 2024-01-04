78a7b77357eae73a9938ff5dd4c2de385259216d
Donisanjaya27 <doni@gmail.co.id>
2024-01-04 14:08:52 +0700

    componen

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,49 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
-function App() {
-	const [count, setCount] = useState(0);
-
-	return (
-		<div className="flex justify-center bg-blue-600 min-h-screen items-center">
+import React from "react";
+class Button extends React.Component {
+	render() {
+		return (
 			<button
-				className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+				className="h-10 px-6 font-semibold rounded-md bg-slate-700 text-white"
 				type="submit"
 			>
 				Buy now
 			</button>
+		);
+	}
+}
+
+function ButtonBlack() {
+	return (
+		<button
+			className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+			type="submit"
+		>
+			Buy now
+		</button>
+	);
+}
+
+const ButtonRed = () => {
+	return (
+		<button
+			className="h-10 px-6 font-semibold rounded-md bg-red-700 text-black"
+			type="submit"
+		>
+			Buy now
+		</button>
+	);
+};
+
+function App() {
+	return (
+		<div className="flex justify-center bg-blue-600 min-h-screen items-center">
+			<div className="flex gap-x-3">
+				<Button />
+				<Button />
+				<Button />
+				<ButtonBlack />
+				<ButtonRed />
+			</div>
 		</div>
 	);
 }
