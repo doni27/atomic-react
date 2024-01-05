d79593dc0a2dda3b9adb58934926417a6932b0c3
Donisanjaya27 <doni@gmail.co.id>
2024-01-05 11:43:05 +0700

    atomic design

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,11 @@
 import React from "react";
-import Button from "./components/Elements/Button";
+import LoginPage from "./pages/login";
+import RegisterPage from "./pages/register";
 function App() {
 	return (
 		<div className="flex justify-center min-h-screen items-center">
-			<div className="w-full max-w-xs">
-				<h1 className="text-3xl font-bold mb-2 text-blue-600">
-					Login
-				</h1>
-				<p className="font-medium text-slate-500 mb-8">
-					{" "}
-					Welcome, Please enter your
-					details
-				</p>
-				<form action="">
-					<div className="mb-6">
-						<label
-							htmlFor="email"
-							className="block text-slate-700 text-sm font-bold mb-2"
-						>
-							Email
-						</label>
-						<input
-							type="text"
-							className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder: opacity-50"
-							placeholder="example@email.com"
-						/>
-					</div>
-					<div className="mb-6">
-						<label
-							htmlFor="email"
-							className="block text-slate-700 text-sm font-bold mb-2"
-						>
-							Password
-						</label>
-						<input
-							type="text"
-							className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder: opacity-50"
-							placeholder="****"
-						/>
-					</div>
-				</form>
-				<Button classname="bg-blue-600 w-full">
-					Login
-				</Button>
-			</div>
+			{/* <LoginPage></LoginPage> */}
+			<RegisterPage></RegisterPage>
 		</div>
 	);
 }
